41319c2e94b10c82f923f5fc8f86839480a50a0e
juank218 <juancaquines@gmail.com>
2024-07-31 19:34:12 -0500

    add mult function

--- main.py
+++ main.py
@@ -24,3 +24,17 @@ def resta(a, b):
         _type_: _description_
     """
     return a- b
+    
+    
+    
+def multiplicacion(a, b):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return a * b
